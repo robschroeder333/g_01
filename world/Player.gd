40a7cb6dc89a1f1fe9135c3245f1cb7f5985cfa4
robschroeder333 <theguyonthecouch7@gmail.com>
2019-05-08 16:55:26 -0700

    feat: add terminal velocity check for kinematic body

--- world/Player.gd
+++ world/Player.gd
@@ -1,6 +1,9 @@
 extends KinematicBody
 
-var speed = 300
+var walk = 300
+var run = 500
+var gravity = 2
+var terminalVelocity = 3
 var direction = Vector3()
 
 func _ready():
@@ -12,6 +15,7 @@ func _physics_process(delta):
 	handleMovement(delta)
 	
 func handleMovement(d):
+	var speed = run # TODO: set walk vs run here
 	direction = Vector3(0,0,0)
 	if Input.is_action_pressed("ui_left"):
 		direction.x -= 1
@@ -21,9 +25,34 @@ func handleMovement(d):
 		direction.z -= 1
 	if Input.is_action_pressed("ui_down"):
 		direction.z += 1
-	direction = direction * speed * d
-	move_and_slide(direction)
+	direction.y -= gravity 
+	var movement = direction * d
+	movement = applyTV(movement, d) * speed # make more like actual gravity
+	move_and_slide(movement)
 	
-func gravity():
+func applyTV(movement, d):
+	var tv = terminalVelocity * d
+	var positiveNegative = 0
+	if abs(movement.y) > tv:
+		if movement.y > 0: 
+			positiveNegative = 1
+		else:
+			positiveNegative = -1 
+		movement.y = tv * positiveNegative
+	if abs(movement.x) > tv:
+		if movement.x > 0: 
+			positiveNegative = 1
+		else:
+			positiveNegative = -1 
+		movement.x = tv * positiveNegative
+	if abs(movement.z) > tv:
+		if movement.z > 0: 
+			positiveNegative = 1
+		else:
+			positiveNegative = -1 
+		movement.z = tv * positiveNegative
+	return movement
+	
+	
 	
 	
