dd013cb63904e10080eb13523d051461c23427d3
robschroeder333 <theguyonthecouch7@gmail.com>
2019-07-30 23:56:44 -0700

    feat(camera): add input for analog sticks

--- world/Player.gd
+++ world/Player.gd
@@ -4,6 +4,9 @@ var walk = 3
 var run = 3
 var terminalVelocity = 3
 var direction = Vector3()
+var left = Vector2()
+var right = Vector2()
+var grounded
 
 func _ready():
 	# Called when the node is added to the scene for the first time.
@@ -11,11 +14,12 @@ func _ready():
 	pass
 
 func _physics_process(delta):
+	grounded = $GroundRay.is_colliding()
 	handleMovement(delta)
+	analogInput()
 	
 func handleMovement(d):
 	var speed = run # TODO: set walk vs run here
-	var grounded = $GroundRay.is_colliding()
 	direction = Vector3(0,0,0)
 	if grounded:
 		if Input.is_action_pressed("i_left"):
@@ -29,3 +33,15 @@ func handleMovement(d):
 		apply_central_impulse(direction)
 	if Input.is_action_just_pressed("i_jump") && grounded:
 		set_axis_velocity(Vector3(0,15,0))
+		
+func handleCamera(l, r):
+	var combined = l + r
+	# TODO: log testing
+
+func analogInput():
+	left = Vector2(0,0)
+	right = Vector2(0,0)
+	left.x = Input.get_action_strength("i_L_right") - Input.get_action_strength("i_L_left")
+	left.y = Input.get_action_strength("i_L_up") - Input.get_action_strength("i_L_down")
+	right.x = Input.get_action_strength("i_R_right") - Input.get_action_strength("i_R_left")
+	right.y = Input.get_action_strength("i_R_up") - Input.get_action_strength("i_R_down")
